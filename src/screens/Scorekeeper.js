0719a80b05feec2dd493f8b65977e8bccfb35657
M LavaT <mwhlavaty@gmail.com>
2023-11-12 11:56:09 +0700

    table work

--- src/screens/Scorekeeper.js
+++ src/screens/Scorekeeper.js
@@ -20,24 +20,26 @@ export default function Scorekeeper({ players }) {
   };
 
   return (
-    <div className="m-2 h-screen text-lg">
-      <table className="width-full">
-        <tr>
-          <th>Player</th>
-          <th>Round Score</th>
-          <th>Total Score</th>
-        </tr>
-        {players.map((player, i) => (
-          <tr>
-            {" "}
-            <td className="w-1/3">{player.name}</td>
-            <td>
-              <input className="w-16"></input>
-            </td>
-            <td>{player.score}</td>
+    <div className="flex flex-col h-screen m-2 text-lg">
+      <table className="table-auto">
+        <thead>
+          <tr className="border p-2">
+            <th className="border p-2 w-1/3">Player</th>
+            <th className="border p-2 w-1/3">Round Score</th>
+            <th className="border p-2 w-1/3">Total Score</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {players.map((player, i) => (
+            <tr className="border p-2">
+              <td className="border p-2">{player.name}</td>
+              <td className="border p-2">{player.name}</td>
+              <td className="border p-2">{player.name}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
+      
       <button
         className="font-bold rounded-lg w-36 p-2 mt-4 bg-violet-600 border-2 border-white "
         onClick={handleRoundSubmit}
